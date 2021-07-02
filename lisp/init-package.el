70c9af18376d20f2f9693d7c3eca8e445a843dc0
Yog-Sothoth <yog@arch.localdomain>
2021-07-02 13:15:56 +0800

    my config

---
+++ lisp/init-package.el
@@ -0,0 +1,90 @@
+;;===============
+;;=restart emacs=
+;;===============
+(use-package restart-emacs)
+
+;;================
+;;=evil(vim) mode=
+;;================
+(use-package evil
+  :init (evil-mode 1))
+
+;;=====
+;;=ivy=
+;;=====
+(use-package ivy
+  :defer 1
+  :demand
+  :hook (after-init . ivy-mode)
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t
+        ivy-initial-inputs-alist nil
+        ivy-count-format "%d/%d "
+        enable-recursive-minibuffers t
+        ivy-re-builders-alist '((t . ivy--regex-ignore-order))))
+
+;;=========
+;;=counsel=
+;;=========
+(use-package counsel
+  :after (ivy)
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-c f" . counsel-recentf)
+         ("C-c g" . counsel-git)))
+
+;;========
+;;=swiper=
+;;========
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper-isearch-backward))
+  :config (setq swiper-action-recenter t
+                swiper-include-line-number-in-search t))
+
+;;=========
+;;=company=
+;;=========
+(use-package company
+  :config
+  (setq company-dabbrev-code-everywhere t
+	company-dabbrev-code-modes t
+	company-dabbrev-code-other-buffers 'all
+	company-dabbrev-downcase nil
+	company-dabbrev-ignore-case t
+	company-dabbrev-other-buffers 'all
+	company-require-match nil
+	company-minimum-prefix-length 2
+	company-show-numbers t
+	company-tooltip-limit 20
+	company-idle-delay 0
+	company-echo-delay 0
+	company-tooltip-offset-display 'scrollbar
+	company-begin-commands '(self-insert-command))
+  (push '(company-semantic :with company-yasnippet) company-backends)
+  :hook ((after-init . global-company-mode)))
+
+;;==========
+;;=flycheck=
+;;==========
+(use-package flycheck
+  :hook (prog-mode . flycheck-mode))
+
+;;======
+;;=crux=
+;;======
+(use-package crux
+  :bind (("C-a" . crux-move-beginning-of-line)
+         ("C-c ^" . crux-top-join-line)
+         ("C-x ," . crux-find-user-init-file)
+         ("C-c k" . crux-smart-kill-line)))
+
+;;============
+;;=ace window=
+;;============
+(use-package ace-window
+    :bind (("M-o" . 'ace-window)))
+
+(provide 'init-package)
